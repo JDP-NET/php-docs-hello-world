f4d57991ad0e400882536f003c4a6a31a9eecd97
JDP-NET <60826456+JDP-NET@users.noreply.github.com>
2024-02-19 17:23:31 -0500

    curl-2

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
-
-// Make a cURL request to ipinfo.io to retrieve the location information
-$ch = curl_init("https://ipinfo.io/{$ip}/json");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
-
-// Decode the JSON response and display the location information
-$details = json_decode($response);
+$details = file_get_contents("https://ipinfo.io/{$ip}/json");
+$details = json_decode($details);
 echo "<h2>Your Location: " . $details->city . ", " . $details->country . "</h2>";
 ?>
